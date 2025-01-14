026208bb762663b93e647e446b1b1c28e1a86e18
Jonathan Díaz <jonathan.diazm5@gmail.com>
2025-01-13 23:19:52 -0500

    feat: Enhance error handling in fetch_page_content function for improved HTTP request feedback

--- src/utils.py
+++ src/utils.py
@@ -22,8 +22,12 @@ import requests
 def fetch_page_content(url):
     """Fetches the HTML content of the page."""
     response = requests.get(url)
-    response.raise_for_status()
-    return response.text
+    try:
+        response.raise_for_status()
+        print("Request to the page was successful.")
+        return response.text
+    except requests.exceptions.HTTPError as e:
+        print(f"Error HTTP: {e}")
 
 
 # Save the HTML content of the infobox in a file for each block
