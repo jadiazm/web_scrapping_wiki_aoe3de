6ad13ea5cb1f8a7b22b7ba767b366125877bb551
Jonathan Díaz <jonathan.diazm5@gmail.com>
2025-02-09 15:36:25 -0500

    refactor: Simplify unit URL extraction and enhance infobox data retrieval

--- src/unit_data_scraper.py
+++ src/unit_data_scraper.py
@@ -22,13 +22,11 @@ project_path = os.path.abspath(".")
 
 sys.path.append(project_path)
 
-from urllib.parse import urlparse
-
 from bs4 import BeautifulSoup as bs
 from tqdm import tqdm
 
 from src.scrap_hrefs import get_section_hrefs
-from src.scrap_infobox import extract_unit_data, find_infobox
+from src.scrap_infobox import extract_unit_data
 from src.utils import fetch_page_content
 
 
@@ -47,41 +45,38 @@ def get_units_urls(url) -> list:
     h2s = content.find_all('h2', recursive=False)
     sections = [h2.find('span', class_='mw-headline', recursive=False).text for h2 in h2s]
     # section_unit_hrefs = {section: get_units_hrefs(section, content) for section in sections}
-    units_urls = {
-        unit: url + href
+    units_urls = [
+        url + href
         for section in sections
-        for unit, href in get_section_hrefs(section, content).items()
-    }
+        for href in get_section_hrefs(section, content).values()
+    ]
 
     return units_urls
 
 
 # 2. Transform: Process the data into a structured format
-def scrape_unit_data(unit, units_urls) -> list:
-
-    TARGET_GAME = "Age of Empires III"
+def get_infoboxes(url, target_game):
 
     try:
-
-        url = units_urls[unit]
 
         unit_html = fetch_page_content(url)
         unit_soup = bs(unit_html, "html.parser")
         infoboxes = unit_soup.find_all("aside", class_="portable-infobox")
 
-        fragment_id = urlparse(url).fragment
+        valid_infoboxes = []
+        for infobox in infoboxes:
+            
+            game_div = infobox.find("div", class_="pi-data-value")
+            
+            if not game_div:
+                continue
 
-        norm_fragment_id = norm_string(fragment_id) if fragment_id else None
-        norm_target_game = norm_string(TARGET_GAME)
-        norm_unit = norm_string(unit)
+            game = game_div.text.strip()
 
-        unit_to_search = (norm_unit if norm_fragment_id == norm_target_game else norm_fragment_id) or norm_unit
+            if target_game in game:
+                valid_infoboxes.append(infobox)
 
-        infobox = find_infobox(unit_to_search, infoboxes)
-
-        unit_data = extract_unit_data(infobox)
-
-        return unit_data
+        return valid_infoboxes
 
     except Exception as e:
         raise Exception(f"Error: {e}")
@@ -96,22 +91,34 @@ def load_data(data, filename):
 
 # ETL Integration
 def etl_process(url, output_file):
+
+    TARGET_GAME = "Age of Empires III"
+
     print("Starting extraction process...")
+
     units_urls = get_units_urls(url)
+
     print(f"{len(units_urls)} unit URLs available for scraping")
+
     if not units_urls:
         print("No unit URLs were obtained")
     
-    # for unit, url in tqdm(all_hrefs.items(), colour="blue"):
     print("Extracting data from the URLs...")
-    data = {}
-    for unit in units_urls.keys():
-        try:
-            data[unit] = scrape_unit_data(unit, units_urls)
-        except Exception as e:
-            print(f"Extraction error for unit: {unit} - {e}")
+    
+    data = []
+    for url in tqdm(units_urls, desc="Scraping", unit="units", colour="green"):
+        infoboxes = get_infoboxes(url, TARGET_GAME)
+        for infobox in infoboxes:
+            try:
+                unit_data = extract_unit_data(infobox)
+                data.append(unit_data)
+            except Exception as e:
+                print(f"\nExtraction error for url '{url}': {e}")
+    
     print(f"Extracted data for {len(data)} units")
+
     load_data(data, output_file)
+
 
 if __name__ == "__main__":
     # Execute the ETL process
