193fa6f64f61832b4904ff646d3492978d792021
Jonathan Díaz <jonathan.diazm5@gmail.com>
2025-02-09 15:51:05 -0500

    refactor: Move `get_section_hrefs` function from `scrap_hrefs.py` to `utils.py`

--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,13 @@
 import os
 
 import requests
+from bs4 import Tag
+
+# import sys
+
+# project_path = os.path.abspath("..")
+
+# sys.path.append(project_path)
 
 
 def fetch_page_content(url):
@@ -42,3 +49,35 @@ def save_infobox_blocks_html(infobox, project_path):
             row_path = os.path.join(folder_path, f"{label}.html")
             with open(row_path, "w") as f:
                 f.write(str(row))
+
+
+def get_section_hrefs(section: str, content: Tag) -> dict:
+    """
+    Get the hyperlinks of the units for each section
+
+    Parameters:
+    ---
+    section (str): 
+        Section of units to get hyperlinks
+    content (Tag): 
+        HTML content that contains the section of units
+
+    Returns:
+    ---
+    dict: 
+        Dictionary with the unit names as keys and the hyperlinks as values
+    """
+        
+    h2s = content.find_all("h2", recursive=False)
+    for h2 in h2s:
+        if h2.find("span", class_="mw-headline", recursive=False).text == section:
+            ul = h2.find_next_sibling("ul")
+            lis = ul.find_all("li", recursive=False)
+            section_units = {}
+            for li in lis:
+                anchor = li.find("a", recursive=False, class_=lambda x: x != "image")
+                if anchor is not None:
+                    unit_name = anchor.text
+                    unit_url = anchor.get("href")
+                    section_units[unit_name] = unit_url
+            return section_units
